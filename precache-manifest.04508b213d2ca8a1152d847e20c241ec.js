e8aa0fc6beac99a7bc2a6a8b9ccba77df10ce42f
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-04-08 21:52:52 -0600

    Updates

---
+++ precache-manifest.04508b213d2ca8a1152d847e20c241ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "f4f5224c7ba0d638de5d",
+    "url": "/ClickyGame/static/js/main.77b5dfc0.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ClickyGame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "f4f5224c7ba0d638de5d",
+    "url": "/ClickyGame/static/css/main.87f3ae75.chunk.css"
+  },
+  {
+    "revision": "352233f1a5de1707176a303f604f4ccb",
+    "url": "/ClickyGame/index.html"
+  }
+];
